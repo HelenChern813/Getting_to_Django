5a3ff3d442b3e538234bd68b87a82e03dee495bf
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-04-01 16:45:18 +0500

    Создание моделей Product, category. Создание и применение миграций

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование",
+        help_text="Введите название продукта",
+    )
+    description = models.CharField(
+        max_length=250, verbose_name="Описание", help_text="Введите описание продукта"
+    )
+    photo = models.ImageField(
+        upload_to="catalog/photo",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите изоброжение продукта",
+    )
+    category = models.CharField(max_length=150, verbose_name="Категория")
+    price = models.IntegerField(verbose_name="Цена за покупку")
+    created_at = models.DateField(max_length=150, verbose_name="Дата создания")
+    updated_at = models.DateField(
+        max_length=150, verbose_name="Дата последнего изменения"
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.description} {self.price}"
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "price"]
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Опишите категорию", blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.description}"
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
