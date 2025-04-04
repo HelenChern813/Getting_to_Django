717386767c95669bf1e1aa38d51c1676db6ad2dc
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-04-04 12:04:02 +0500

    Создан базовый шаблон для .html, контроллер для конкретного продукта и описан путь для контроллера

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_list
 from catalog.apps import CatalogConfig
 
 
 app_name = CatalogConfig.name
 
-urlpatterns = [path("", home, name="home"), path("contacts", contacts, name="contacts")]
+urlpatterns = [
+    path("", home, name="home"),
+    path("contacts", contacts, name="contacts"),
+    path('product_list/<int:id_product>', product_list, name='product_list')
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
@@ -7,3 +8,13 @@ def home(request):
 
 def contacts(request):
     return render(request, "contacts.html")
+
+
+def product_list(request, id_product):
+
+    product = Product.objects.get(id=id_product)
+
+    context = {
+        'product': product
+    }
+    return render(request, 'product_list.html', context)
