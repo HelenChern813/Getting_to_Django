c37b2fec593223d8271387d85ca49e7d3d62005c
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-04-05 21:45:43 +0500

    Создание шаблонов страниц, регистрация модели в админке, создание и применение миграций

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Blogs
 
-# Register your models here.
+
+@admin.register(Blogs)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "contents", "preview", "created_at", "is_activ", "count_views")
+    list_filter = ("title", "created_at")
+    search_fields = (
+        "title",
+        "contents",
+        "created_at",
+        "is_activ"
+    )
