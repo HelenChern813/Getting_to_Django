934b55351722e3630a9faaeb25fb308acef81efb
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-04-05 20:36:50 +0500

    Описаны контроллеры и маршруты приложения Blogs

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-# from blogs.views import
+from blogs.views import BlogsUpdateView, BlogsCreateView, BlogsDeleteView, BlogsDetailView, BlogsListView
 from blogs.apps import BlogsConfig
 
 
 app_name = BlogsConfig.name
 
-# urlpatterns = [
-#     path("", ProductListView.as_view(), name="product_list"),
-#     path("contacts/", contacts, name="contacts"),
-#     path('product/<int:pk>/', ProductDtailView.as_view(), name='product_detail'),
-#     path('product/new/', ProductCreateView.as_view(), name='product_create'),
-#     path('product/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
-#     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
-# ]
+urlpatterns = [
+    path("publications/", BlogsListView.as_view(), name="publication_list"),
+    path('publication/<int:pk>/', BlogsDetailView.as_view(), name='publication_detail'),
+    path('publication/new/', BlogsCreateView.as_view(), name='publication_create'),
+    path('publication/<int:pk>/edit/', BlogsUpdateView.as_view(), name='publication_edit'),
+    path('publication/<int:pk>/delete/', BlogsDeleteView.as_view(), name='publication_delete')
+]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from blogs.models import Blogs
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+
+class BlogsCreateView(CreateView):
+    model = Blogs
+    fields = ['title', 'contents', 'preview']
+    template_name = 'publication_form.html'
+    success_url = reverse_lazy('blogs:publication_list')
+
+
+class BlogsDetailView(DetailView):
+    model = Blogs
+    template_name = 'publication_detail.html'
+    context_object_name = 'blogs'
+
+
+class BlogsUpdateView(UpdateView):
+    model = Blogs
+    fields = ['title', 'contents', 'preview']
+    template_name = 'publication_form.html'
+    success_url = reverse_lazy('blogs:publication_list')
+
+
+class BlogsDeleteView(DeleteView):
+    model = Blogs
+    template_name = 'publication_confirm_delete.html'
+    success_url = reverse_lazy('blogs:publication_list')
+
+
+class BlogsListView(ListView):
+    model = Blogs
+    template_name = 'publication_list.html'
+    context_object_name = 'blogs'
