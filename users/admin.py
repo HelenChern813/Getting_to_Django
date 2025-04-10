4586c12ca050aec27359b2990eea42f374ae1055
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-04-11 04:11:24 +0500

    Добавление модли в админку

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "phone_number",
+        "country",
+        "avatar",
+        "username",
+        "first_name",
+        "last_name",
+        "is_active",
+    )
+    list_filter = ("email", "country", "username", "is_active")
+    search_fields = ("country", "country", "phone_number")
